ada9e9f2a845f81e11dd5f5f1a71ae3638c19ec0
Teeinn0730 <57168985+Teeinn0730@users.noreply.github.com>
2021-07-12 01:27:45 +0800

    Add files via upload

--- AverageSkinMeshNormals.cs
+++ AverageSkinMeshNormals.cs
@@ -1,22 +1,32 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class AverageSkinMeshNormals : MonoBehaviour
 {
     public SkinnedMeshRenderer[] meshSources;
     private static readonly Vector3 zeroVec = Vector3.zero;
-    // Use this for initialization
-    void Start()
+
+    void OnGUI()
+    {
+        GUI.BeginGroup(new Rect(0 , 0 , 300 , 300));
+        GUI.Box(new Rect(0, 0, 300, 300), "界面");
+        if (GUI.Button(new Rect(40, 100, 200, 60), "Excuted！"))
+        { 
+            Excuted();
+        }
+        GUI.EndGroup();
+    }
+
+    void Excuted()
     {
         foreach (SkinnedMeshRenderer meshSource in meshSources)//讓新的meshSource去跑舊的meshSources中所有陣列元素
         {
             Vector3[] verts = meshSource.sharedMesh.vertices;//每個點都擁有pos的三維數據
             Vector3[] normals = meshSource.sharedMesh.normals;
             VertInfo[] vertInfo = new VertInfo[verts.Length];//創建一個新的陣列元素 讓編號對應新建的verts.Length
-            for (int i = 0; i < verts.Length; i++)
+
+            for (int i = 0, iMax = verts.Length; i < iMax; i++)
             {
                 vertInfo[i] = new VertInfo()//每個vertInfo都會執行下面的方法
                 {
@@ -25,10 +35,11 @@ public class AverageSkinMeshNormals : MonoBehaviour
                     normal = normals[i]
                 };
             }
-            var groups = vertInfo.GroupBy(x => x.vert);
+
+            var theGroups = vertInfo.GroupBy(x => x.vert);
             VertInfo[] processedVertInfo = new VertInfo[vertInfo.Length];
             int index = 0;
-            foreach (IGrouping<Vector3, VertInfo> group in groups)
+            foreach (IGrouping<Vector3, VertInfo> group in theGroups)
             {
                 Vector3 avgNormal = zeroVec;
                 foreach (VertInfo item in group)
@@ -57,15 +68,16 @@ public class AverageSkinMeshNormals : MonoBehaviour
                 int origIndex = info.origIndex;
                 Vector3 normal = info.averagedNormal;
                 //Color normColor = new Color(normal.x, normal.y, normal.z, 1);
-                Vector3 normColor2 = new Vector3(normal.x , normal.y , normal.z);
+                Vector3 normColor2 = new Vector3(normal.x, normal.y, normal.z);
                 vector3s[origIndex] = normColor2;
                 //colors[origIndex] = normColor;
                 //Debug.Log(normColor2);
             }
-            
+
             //meshSource.sharedMesh.SetUVs( 1,vector3s );
             meshSource.sharedMesh.tangents = vector3s;
             //meshSource.sharedMesh.colors = colors;
+            Debug.Log("有寫入");
         }
     }
     private struct VertInfo // 建立函數
